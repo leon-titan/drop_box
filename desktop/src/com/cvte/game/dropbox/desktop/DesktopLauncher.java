7601ed962aea9f728bc656fb215dabc0f172966c
leon_titan <gzq053782@163.com>
2014-08-04 21:36:03 +0800

    Initial commit

---
+++ desktop/src/com/cvte/game/dropbox/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.cvte.game.dropbox.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.cvte.game.dropbox.MyGdxGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new MyGdxGame(), config);
+	}
+}
