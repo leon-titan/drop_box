13370cae4011e4f7ad1803e42a1b4762c2bae3e1
leon_titan <gzq053782@163.com>
2014-08-06 13:02:34 +0800

    build the frame work

---
+++ core/src/com/cvte/game/dropbox/BoxGame.java
@@ -0,0 +1,7 @@
+package com.cvte.game.dropbox;
+
+/**
+ * Created by cvtpc on 2014/8/5.
+ */
+public class BoxGame {
+}
---
+++ core/src/com/cvte/game/dropbox/game/BackgroundActor.java
@@ -0,0 +1,7 @@
+package com.cvte.game.dropbox.game;
+
+/**
+ * Created by cvtpc on 2014/8/5.
+ */
+public class BackgroundActor {
+}
