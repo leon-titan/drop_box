bc2720a83d460d9a55a6b7cd2973cdb44deb9b2b
Boa <boa@boadembp.workgroup>
2014-08-31 00:38:05 +0800

    modify the ios startup code

--- ios/src/com/cvte/game/dropbox/IOSLauncher.java
+++ ios/src/com/cvte/game/dropbox/IOSLauncher.java
@@ -10,7 +10,7 @@ public class IOSLauncher extends IOSApplication.Delegate {
     @Override
     protected IOSApplication createApplication() {
         IOSApplicationConfiguration config = new IOSApplicationConfiguration();
-        return new IOSApplication(new Box2dLightTest(), config);
+        return new IOSApplication(new BoxGame(), config);
     }
 
     public static void main(String[] argv) {
