58200c66099f3a0d110c4aeafaa5e36aa3f94b7f
leon_titan <gzq053782@163.com>
2014-10-16 10:56:18 +0800

    update libgdx to 1.4.1

--- core/src/com/cvte/game/dropbox/game/GameScreen.java
+++ core/src/com/cvte/game/dropbox/game/GameScreen.java
@@ -62,7 +62,7 @@ public class GameScreen implements Screen {
             return;
         }
         state = State.START;
-        riseGroup(170);
+//        riseGroup(170);
     }
 
     public void end() {
@@ -130,7 +130,7 @@ public class GameScreen implements Screen {
 
     @Override
     public void render(float delta) {
-        Gdx.gl.glClearColor(1, 1, 1, 1);
+        Gdx.gl.glClearColor(0, 0.388235f, 0.6745098f, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
         gameStage.act(delta);
--- ios/src/com/cvte/game/dropbox/IOSLauncher.java
+++ ios/src/com/cvte/game/dropbox/IOSLauncher.java
@@ -5,12 +5,13 @@ import org.robovm.apple.uikit.UIApplication;
 
 import com.badlogic.gdx.backends.iosrobovm.IOSApplication;
 import com.badlogic.gdx.backends.iosrobovm.IOSApplicationConfiguration;
+import com.cvte.game.dropbox.BoxGame;
 
 public class IOSLauncher extends IOSApplication.Delegate {
     @Override
     protected IOSApplication createApplication() {
         IOSApplicationConfiguration config = new IOSApplicationConfiguration();
-        return new IOSApplication(new Box2dLightTest(), config);
+        return new IOSApplication(new BoxGame(), config);
     }
 
     public static void main(String[] argv) {
