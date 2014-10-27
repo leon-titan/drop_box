6a3227a0021f7a007926a0b204699946e1277847
leon_titan <gzq053782@163.com>
2014-10-27 22:24:37 +0800

    add end line ctrl

---
+++ core/src/com/cvte/game/dropbox/game/EndLine.java
@@ -0,0 +1,34 @@
+package com.cvte.game.dropbox.game;
+
+import com.badlogic.gdx.scenes.scene2d.Actor;
+import com.badlogic.gdx.scenes.scene2d.Group;
+
+import static com.badlogic.gdx.scenes.scene2d.actions.Actions.*;
+
+/**
+ * Created by CVTEr on 2014/10/27.
+ */
+public class EndLine {
+
+    private static final int END_LINE_NUM = 13;
+    private static final float END_LINE_WIDTH = 55.3F;
+
+    private Group group;
+
+    public EndLine() {
+        group = new Group();
+        for (int i = 0; i < END_LINE_NUM; ++i) {
+            LittleBlock block = new LittleBlock();
+            group.addActor(block);
+
+            block.setPosition(i * END_LINE_WIDTH, 0);
+            block.setColor(1, 1, 1, 0);
+
+            block.addAction(sequence(delay(i * 0.07f), forever(sequence(alpha(0.3f, 0.5f), delay(0.02f), alpha(0.7f, 0.5f), delay(0.02f)))));
+        }
+    }
+
+    public Actor getActor(){
+        return group;
+    }
+}
---
+++ core/src/com/cvte/game/dropbox/game/LittleBlock.java
@@ -0,0 +1,14 @@
+package com.cvte.game.dropbox.game;
+
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.scenes.scene2d.ui.Image;
+
+/**
+ * Created by CVTEr on 2014/10/27.
+ */
+public class LittleBlock extends Image{
+
+    public LittleBlock() {
+        super(new Texture("littleblock.png"));
+    }
+}
